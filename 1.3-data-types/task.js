dca1ed8f26e5c4787f1e1a866c4949a74a09e883
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-23 21:58:11 +0300

    first commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,5 +1,22 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
+    let percent2 = Number(percent);
+    let contribution2 = Number(contribution);
+    let amount2 = Number(amount);
+    if (Number.isNaN(percent2)) {
+    	return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}`;
+    }
+    if (Number.isNaN(contribution2)) {
+    	return `Параметр 'Начальный взнос' содержит неправильное значение ${contribution}`;
+    }
+    if (Number.isNaN(amount2)) {
+    	return `Параметр 'Общая стоимость' содержит неправильное значение ${amount}`;
+    }
+
+    let principal = amount - contribution;
+    let today = new Date();
+    let months = ((date.getFullYear() - today.getFullYear()) * 12) + (date.getMonth() - today.getMonth());
     // return totalAmount;
 }
 

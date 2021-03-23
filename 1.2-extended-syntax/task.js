a78748003ca0939a9ad65e404da2242679fa5532
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-23 20:15:55 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,45 @@
+'use strict';
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = b**2 - 4 * a * c;
+	let x = [];
+
+    if (d === 0) {
+    	x[0] = -b / (2 * a); 
+    } else if (d > 0) {
+    	x[0] = (-b + d**0.5) / 2 * a;
+    	x[1] = (-b - d**0.5) / 2 * a;
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark = 0;
+
+	if (marks.length > 5) {
+		console.log('ВНИМАНИЕ! Вы ввели более 5 оценок!');
+    	marks.splice(5);
+	}
+
+    if (marks.length > 0) {   	
+    	let sum = 0;
+    	for (let i = 0; i < marks.length; i++) {
+    		sum += marks[i];
+    	}
+    	averageMark = sum / marks.length;
+    } 
+ 
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let today = new Date();
+    let age = today.getFullYear() - dateOfBirthday.getFullYear();
+    
+    if (age > 17) {
+    	return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+    	return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+    }
 }

c2bbf1f2d97e5bec54733f0b83e06a123dbf7b7a
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-27 20:09:39 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,47 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function isPalindrome() {
+	console.log(this.toLowerCase().split(' ').join('').split('').join(''));
+	console.log(this.split('').join(''));
+	console.log(this.trim())
+	if (this.toLowerCase().split(' ').join('').split('').reverse().join('') === this.toLowerCase().split(' ').join('').split('').join('')) {
+		return true
+	} else {
+		return false
+	}
 }
 
+// Задача 2
+
+function getAverageMark(marks) {
+	if (marks.length === 0) {
+		return 0;
+	}
+
+	let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+    	sum += marks[i];
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage
+}
+
+// Задача 3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date;
+    let secBirthday = birthday.split('-');
+    let year = Number(secBirthday[0]);
+    let month = Number(secBirthday[1] - 1);
+    let day = Number(secBirthday[2]);
+    birthday = +new Date(year, month, day);
+    let diff = now - birthday;
+    let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+    
+    if (age >= 18) {
+    	return true
+    } else {
+    	return false
+    }
 }

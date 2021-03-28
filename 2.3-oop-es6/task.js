fe52c088e2229cc20e64f301d3a0ee5a1696231d
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-28 10:59:25 +0300

    second commit oop-es6

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,5 +1,5 @@
 class PrintEditionItem {
-	constructor(name, releaseDate, pagesCount, state, type) {
+	constructor(name, releaseDate, pagesCount) {
 		this.name = name;
 		this.releaseDate = releaseDate;
 		this.pagesCount = pagesCount;
@@ -7,14 +7,38 @@ class PrintEditionItem {
 		this.type = null;
 	}
 
-	fix() {
-		this.state = this.state * 1.5;
-		if (this.state < 1) {
-			set state(0);
-		} else if (this.state > 100) {
-			this.state = 100;
+	set state(state) {
+    	this._state = state;
+		if (state < 0) {
+			this._state = 0;
+		} else if (state > 100) {
+			this._state = 100;
 		}
 	}
 
+	get state() {
+		return this._state;
+	}
 
+	fix() {
+		this.state = this.state * 1.5;
+    	return this.state;
+  }
 }
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.book = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.book.push(book)
+		}
+	}
+
+	findBookBy(type, value) {
+		
+	}
+} 

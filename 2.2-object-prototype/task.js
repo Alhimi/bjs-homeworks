e19b7268cf5f43585f69ca6c40f9d40c20fbf48b
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-29 10:05:52 +0300

    second commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -3,18 +3,15 @@
 String.prototype.isPalindrome = function isPalindrome() {
 	console.log(this.toLowerCase().split(' ').join('').split('').join(''));
 	console.log(this.split('').join(''));
-	console.log(this.trim())
-	if (this.toLowerCase().split(' ').join('').split('').reverse().join('') === this.toLowerCase().split(' ').join('').split('').join('')) {
-		return true
-	} else {
-		return false
-	}
+	console.log(this.trim());
+	const source = this.split(' ').join('').toLowerCase().split('');	
+	return source.join('') === source.reverse().join('');
 }
 
 // Задача 2
 
 function getAverageMark(marks) {
-	if (marks.length === 0) {
+	if (!marks.length) {
 		return 0;
 	}
 
@@ -22,22 +19,24 @@ function getAverageMark(marks) {
     for (let i = 0; i < marks.length; i++) {
     	sum += marks[i];
     }
-    let average = sum / marks.length;
-    let roundedAverage = Math.round(average);
+    const average = sum / marks.length;
+    const roundedAverage = Math.round(average);
     return roundedAverage
 }
 
 // Задача 3
 
 function checkBirthday(birthday) {
-    let now = +new Date;
-    let secBirthday = birthday.split('-');
-    let year = Number(secBirthday[0]);
-    let month = Number(secBirthday[1] - 1);
-    let day = Number(secBirthday[2]);
+    const now = +new Date;
+    const secBirthday = birthday.split('-');
+    const year = Number(secBirthday[0]);
+    const month = Number(secBirthday[1] - 1);
+    const day = Number(secBirthday[2]);
+    // const parsedBirthday = new Date(birthday).getTime();
+    // не переназначать аргумент функции
     birthday = +new Date(year, month, day);
-    let diff = now - birthday;
-    let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+    const diff = now - birthday;
+    const age = diff / (1000 * 60 * 60 * 24 * 365.25);
     
     if (age >= 18) {
     	return true

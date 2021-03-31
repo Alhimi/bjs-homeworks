97ff07d777784b9b2fc61541667ada778944e836
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-31 16:17:46 +0300

    forth commit oop-es6

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class PrintEditionItem {
 	constructor(name, releaseDate, pagesCount, state, type) {
 		this.name = name;
@@ -88,38 +90,97 @@ class DetectiveBook extends Book {
 class Library {
 	constructor(name) {
 		this.name = name;
-		this.book = [];
+		this.books = [];
 	}
 
 	addBook(book) {
 		if (book.state > 30) {
-			this.book.push(book)
+			this.books.push(book)
 		}
 	}
 
 	findBookBy(type, value) {
-		for (let i = 0; i < this.book.length; i++){
-	    	if (this.book[i].type === value) {
-		        return this.book[i]
+		for (let i = 0; i < this.books.length; i++){
+	    	if (this.books[i][type] === value) {
+		        return this.books[i];
 	    	}
 		}
+		return null;
 	}
 	
-	giveBookByName() {
-	  
+	giveBookByName(name) {
+		for (let i = 0; i < this.books.length; i++){
+	    	if (this.books[i].name === name) {
+		        let targetBook = this.books[i];
+		        this.books.splice(i, 1)
+		        return targetBook;
+	    	}
+		}
+		return null;
 	}
 } 
 
-// // Задача 3 
 
-// class StudentLog {
-//   	constructor(name) {
-//     	this.name = name;
-//   	}
+// Задача 3 
+
+class StudentLog {
+  	constructor(name) {
+    	this.name = name;
+  	}
   
-//   	getName() {
-//    		return this.name;
-//  	}
+  	getName() {
+   		return this.name;
+ 	}
   
-  
-// }
+	addGrade(grade, subject) {
+		let _grade = Number(grade);
+	 
+		if (!this[subject]) {
+			this[subject] = [];
+		}
+
+		if (1 > _grade || _grade > 6 || Number.isNaN(_grade)) {
+			console.log(`Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.`);
+			return this[subject].length;
+			// let alert = `Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.`;
+			// return `${alert}\n${this[subject].length}`;
+		}
+
+		this[subject].push(Number(grade));
+		return this[subject].length;
+	}  
+
+	getAverageBySubject(subject) {    	
+		if (!this[subject]) {
+		 	return 0;
+	    } 
+
+	    let sum = 0;
+		for (let i = 0; i < this[subject].length; i++) {
+		    sum += this[subject][i];
+		}
+	    return sum / this[subject].length;
+	}
+
+	getTotalAverage() {
+		const subjects = Object.keys(this);
+		if (subjects.length === 1) {
+			return 0;
+		}
+
+		let sum = 0;
+		
+		for (let i = 1; i < subjects.length; i++) {
+			let subject = subjects[i];
+			let subjectMarks = this[subject];
+			let sumSubject = 0;
+			for (let j = 0; j < subjectMarks.length; j++) {
+		    	sumSubject += subjectMarks[j];
+			}
+			sum += sumSubject / subjectMarks.length;
+		}
+		return sum / (subjects.length - 1);
+	}
+}
+
+

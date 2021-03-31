10b9f06dc04087c0aff3397e1ecb186e5fd5cee9
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-03-31 16:28:25 +0300

    forth commit data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -4,13 +4,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let _percent = Number(percent);
     let _contribution = Number(contribution);
     let _amount = Number(amount);
-    if (Number.isNaN(_percent)) {
+    if (Number.isNaN(_percent) || _percent < 0) {
     	return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}`;
     }
-    if (Number.isNaN(_contribution)) {
+    if (Number.isNaN(_contribution) || _contribution < 0) {
     	return `Параметр 'Начальный взнос' содержит неправильное значение ${contribution}`;
     }
-    if (Number.isNaN(_amount)) {
+    if (Number.isNaN(_amount) || _amount < _contribution) {
     	return `Параметр 'Общая стоимость' содержит неправильное значение ${amount}`;
     }
 
@@ -27,8 +27,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 function getGreeting(name) {
   
-	let welcomeName;
-  	name ? welcomeName = name : welcomeName = "Аноним";
+	const welcomeName = name ? name : "Аноним";
   	let greeting = `Привет, мир! Меня зовут ${welcomeName}.`;
   	console.log(greeting);
     return greeting;

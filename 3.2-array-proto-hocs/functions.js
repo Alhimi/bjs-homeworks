c0c5db5edce581501b417a3b22d1fa2609469952
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-04-04 20:13:31 +0300

    first commit array-proto-hocs

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+
+function getNames(arr) {
+  return arr.map(item => item.name);
+}
+
+function getCountReliableWeapons(arr) {
+  reduce
+} 
+
+function hasReliableWeapons() {
+  some
+}
+
+function getReliableWeaponsNames() {
+  map
+}
+
+function getTotalDamage() {
+  reduce
+} 
+
+function bonus() {
+  reduce
+}
--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -2,3 +2,31 @@
 
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
+function getNames(arr) {
+  return arr.map(item => item.name);
+}
+
+function getCountReliableWeapons(arr) {
+  reduce
+} 
+
+function hasReliableWeapons() {
+  some
+}
+
+function getReliableWeaponsNames() {
+  map
+}
+
+function getTotalDamage() {
+  reduce
+} 
+
+function bonus() {
+  reduce
+}
+
+// Задача 2 
+
+
+

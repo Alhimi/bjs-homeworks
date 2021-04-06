69bd899145d27faf4807cfa20a0a24bbe97caa7a
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-04-06 19:16:32 +0300

    second commit array-proto-hocs

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -2,26 +2,47 @@
 
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-function getNames(arr) {
-  return arr.map(item => item.name);
+function getNames(weapons) {
+  return weapons.map(item => item.name);
 }
 
-function getCountReliableWeapons(arr) {
-  reduce
+// не понимаю, как правильно передать массив оружия в функции
+
+function getCountReliableWeapons(weapons, init) {
+  return weapons.reduce(function (previosValue, item){
+    if (item.durability > init) {
+      previosValue++;
+    }
+    return previosValue;
+  }, 0);
 } 
 
-function hasReliableWeapons() {
-  some
+function hasReliableWeapons(weapons, init) {
+  return weapons.some(item => item.durability > init);
 }
 
-function getReliableWeaponsNames() {
-  map
+function getReliableWeaponsNames(weapons, init) {
+    return weapons.map(function(item) {
+    if (item.durability > init) { 
+  	return item.name;
+    }
+  }).filter(item => item !== undefined);
 }
 
-function getTotalDamage() {
-  reduce
+function getTotalDamage(weapons) {
+  return weapons.reduce(function (previosValue, item){
+    previosValue = previosValue + item.initDurability - item.durability; 
+    return previosValue;
+  }, 0);
 } 
 
-function bonus() {
-  reduce
+function bonus(arr, num) {
+  let count = 0;
+  return arr.reduce(function(previosValue, item){
+    count += item;
+  	if (num >= count) {
+  		previosValue++
+  	}
+    return previosValue;
+  }, 0);
 }

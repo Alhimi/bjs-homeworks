08aa04795e8d0307f1299271053854ee521dcad4
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-04-12 18:50:53 +0300

    forth commit array-proto-hocs

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,11 +1,27 @@
 'use strict'
 
-function memorize() {
-	function innerResultFunc() {
-  };
-  return innerResultFunc;
-}
+// function memorize() {
+// 	function innerResultFunc() {
+//   };
+//   return innerResultFunc;
+// }
 
+// function memorize2(a, b, c, d) {
+// 	function innerResultFunc(a, b, c, d) {
+//     console.log(a, b, c, d)
+//   };
+//   return innerResultFunc;
+// }
+
+// function memorize3(func) {
+//   function innerResultFunc(a) {
+//     return func(a);
+//   };
+//   return innerResultFunc;
+// }
+
+// const resultFunction = memorize3(a => a ** 2);
+// resultFunction(2); // <= должно вызываться без ошибок
 
 
 // function sleep(milliseconds) {
@@ -26,27 +42,18 @@ function memorize() {
 // /////////////
 
 function compareArrays( arr1, arr2 ) {
-	arr1.every
+	if (arr1.length === arr2.length && arr1.every(num => num === arr2[arr1.indexOf(num)])) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-// compareArrays([8, 9], [6]); // false, разные значения
-// compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
-// compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
-// compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
-// compareArrays([8, 1, 2], [8, 1, 2]); // true
 
 // //////////
 
-// function memorize(fn, limit) {
 
-// }
+// непонимаю, как можно обЪявить массив в функции так, что бы щт при каждом вызове не переобЪявлялся!?
 
-// const mSum = memorize(sum, 5); // 5 результатов может хранится в памяти
+const memory = []
 
-// // Вызов этих функций даёт один и тот же результат
-// sum(3, 4); // 7
-// /* 
-//   разница только в том, что mSum запоминает результат (7)
-//   и повторно не делает вычисления
-//  */
-// mSum(3, 4); // 7
+

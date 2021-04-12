bbbdf2f80097407eb007121031500695e535db2f
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-04-12 14:36:46 +0300

    third commit array-proto-hocs

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,50 +1,52 @@
 'use strict'
 
-const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
-
-function getNames(weapons) {
-  return weapons.map(item => item.name);
+function memorize() {
+	function innerResultFunc() {
+  };
+  return innerResultFunc;
 }
 
-// не понимаю, как правильно передать массив оружия в функции
 
-function getCountReliableWeapons(weapons, init) {
-  return weapons.reduce(function (previosValue, item){
-    if (item.durability > init) {
-      previosValue++;
-    }
-    return previosValue;
-  }, 0);
-} 
 
-function hasReliableWeapons(weapons, init) {
-  return weapons.some(item => item.durability > init)
+// function sleep(milliseconds) {
+// 	let e = new Date().getTime() + milliseconds;
+// 	while (new Date().getTime() <= e) {}
+// }
+
+// ////////
+
+// function sum(...args) {
+// 	// Замедление на половину секунды.
+// 	sleep(100); // Можно использовать другое значение замедления.
+// 	return args.reduce((sum, arg) => {
+// 		return sum += +arg;
+//   	}, 0);
+// }
+
+// /////////////
+
+function compareArrays( arr1, arr2 ) {
+	arr1.every
 }
 
-function getReliableWeaponsNames(weapons, init) {
-    return weapons.map(function(item) {
-    if (item.durability > init) { 
-  	return item.name;
-    }
-  }).filter(item => item !== undefined);
-}
+// compareArrays([8, 9], [6]); // false, разные значения
+// compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
+// compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
+// compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
+// compareArrays([8, 1, 2], [8, 1, 2]); // true
 
-function getTotalDamage(weapons) {
-  return weapons.reduce(function (previosValue, item){
-    previosValue = previosValue + item.initDurability - item.durability; 
-    return previosValue;
-  }, 0);
-} 
+// //////////
 
-function bonus(arr, num) {
-  let count = 0;
-  return arr.reduce(function(previosValue, item){
-    count += item;
-    console.log(item)
-    console.log(count)
-  	if (num >= count) {
-  		previosValue++
-  	}
-    return previosValue;
-  }, 0);
-}
+// function memorize(fn, limit) {
+
+// }
+
+// const mSum = memorize(sum, 5); // 5 результатов может хранится в памяти
+
+// // Вызов этих функций даёт один и тот же результат
+// sum(3, 4); // 7
+// /* 
+//   разница только в том, что mSum запоминает результат (7)
+//   и повторно не делает вычисления
+//  */
+// mSum(3, 4); // 7

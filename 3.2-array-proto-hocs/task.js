d0dc674a90870f1d7e4fb993beb512bfa31bd353
Алексей Гороховацкий <zar.goroh@yandex.ru>
2021-04-16 11:31:22 +0300

    ninth commit array-proto-hocs

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,37 +1,19 @@
 'use strict'
 
-// function memorize() {
-// 	function innerResultFunc() {
-//   };
-//   return innerResultFunc;
-// }
-
-// function memorize2(a, b, c, d) {
-// 	function innerResultFunc(a, b, c, d) {
-//     console.log(a, b, c, d)
-//   };
-//   return innerResultFunc;
-// }
-
-
-// const resultFunction = memorize(a => a ** 2);
-// resultFunction(2); // <= должно вызываться без ошибок
-
-
-// function sleep(milliseconds) {
-// 	let e = new Date().getTime() + milliseconds;
-// 	while (new Date().getTime() <= e) {}
-// }
+function sleep(milliseconds) {
+	let e = new Date().getTime() + milliseconds;
+	while (new Date().getTime() <= e) {}
+}
 
 // ////////
 
-// function sum(...args) {
-// 	// Замедление на половину секунды.
-// 	sleep(100); // Можно использовать другое значение замедления.
-// 	return args.reduce((sum, arg) => {
-// 		return sum += +arg;
-//   	}, 0);
-// }
+function sum(...args) {
+	// Замедление на половину секунды.
+	sleep(100); // Можно использовать другое значение замедления.
+	return args.reduce((sum, arg) => {
+		return sum += +arg;
+  	}, 0);
+}
 
 // /////////////
 
@@ -41,37 +23,30 @@ function compareArrays(arr1, arr2) {
 
 // //////////
 
+const memory = [];
 
-// непонимаю, как можно обЪявить массив в функции так, что бы он при каждом вызове не переобЪявлялся!?
-const memory = []
-
-  	// тоже никак не понимаю, как метод some соеденить с compareArrays так, 
-  	// что бы compareArrays правильно получал аргументы. 
-  	// пробовал по другому, но не получилось >>>
-  	
-// function memorize(func, limit) {
-//   function innerResultFunc(...args) {
-  	// if(memory.some(arr.args === [...args]) {
-  	// 	let result = memory.find(arr.args === [...args]).result;
-  	// } else {
-  	// 	let result = func(...args); 
-  	// 	memory.push({'args': ...args, 'result': result});	
-  	// }
-   //  if(memory.length > limit) {
-   //  	memory.shift;
-   //  }
-   //  return result;
-   //  }
-//   }
-//   return innerResultFunc;
-// }
-
-function memorize(func) {
+function memorize(func, limit) {
   function innerResultFunc(...args) {
-    return func(...args);
-  };
+  	if(memory.find(arr => arr.args === [...args])) {
+      return memory.find(arr => arr.args === [...args]).result;
+  	} else {
+  		let result = func(...args); 
+  		memory.push({'args': [...args], 'result': result});	
+  	}
+    if(memory.length > limit) {
+    	memory.shift();
+    }
+    return result;
+  }
   return innerResultFunc;
 }
+
+// function memorize(func) {
+//   function innerResultFunc(...args) {
+//     return func(...args);
+//   };
+//   return innerResultFunc;
+// }
 
 
 
